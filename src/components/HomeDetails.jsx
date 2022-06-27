880ee4de212499aa7e8267e509e5c7f9e21b58e6
Anderson Marlon <anderson18.marlon@gmail.com>
2022-06-27 18:47:05 -0300

    Added: HomeDetails

---
+++ src/components/HomeDetails.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const HomeDetails = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 20rem;
+
+`
+
+export default HomeDetails
